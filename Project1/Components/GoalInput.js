8fce08d4988cca15cd3f186ffe118017bb9951ee
ambi hidalgo <ambio.pk@gmail.com>
2023-06-28 16:33:12 -0700

    Add modal, app styling finish app

--- Project1/Components/GoalInput.js
+++ Project1/Components/GoalInput.js
@@ -1,4 +1,4 @@
-import {View, Text, TextInput, Button, StyleSheet} from 'react-native'
+import {View, Text, TextInput, Button, StyleSheet, Modal, Image} from 'react-native'
 import { useState } from 'react';
 function GoalInput(props){
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -12,15 +12,28 @@ function GoalInput(props){
   }
 
   return (
+    <Modal visible={props.visible} animationType="slide">
     <View style={styles.inputContainer}>
+      <Image style={styles.image} 
+      source={require('../assets/images/goal.png')}/>
        <TextInput 
        style={styles.textInput}
        placeholder='Your Course Goal!'
        onChangeText={goalInputHandler}
        value={enteredGoalText}
        />
-        <Button title='Add Goal' onPress={addGoalHandler}></Button>
+       <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button title='Cancel' color= '#f31282'onPress={props.onCancel} />
+        </View>
+        <View style={styles.button}>
+          <Button title='Add Goal' onPress={addGoalHandler} color='#b180f0'></Button>
+        </View>
+
+       </View>
+
       </View>
+      </Modal>
   );
     
   
@@ -30,20 +43,34 @@ export default GoalInput
 const styles = StyleSheet.create ({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: 'grey'
+    padding: 16,
+    backgroundColor: '#311b6b', 
   
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#b5d7d5',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
+    borderColor: '#e4d0ff',
+    backgroundColor: '#e4d0ff',
+    color: '#120438',
+    borderRadius: 6,
+    width: '100%',
+    padding: 16
   },
+  buttonContainer :{
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8
+
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20
+  }
 
 })
