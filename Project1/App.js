0d3288309e5d5a5b76b0169ed24d67f9dabeaa6b
ambi hidalgo <ambio.pk@gmail.com>
2023-06-28 14:58:35 -0700

    update git ignore

--- Project1/App.js
+++ Project1/App.js
@@ -16,9 +16,15 @@ export default function App() {
     ])
   }
 
-    function deleteGoalHandler(){
-      console.log('DELETE')
-    }
+    function deleteGoalHandler(id){
+      setCourseGoals((currentCourseGoals )=> {
+        return currentCourseGoals.filter((goal) => goal.id !== id)
+      });
+
+      }
+
+      
+    
 
   return (
     <View style={styles.appContainer}>
@@ -28,7 +34,10 @@ export default function App() {
         <FlatList 
         data={courseGoals} 
         renderItem={(itemData) => {
-          return <GoalItem text={itemData.item.text} onDeleteItem={deleteGoalHandler}/>;
+          return <GoalItem 
+          text={itemData.item.text} 
+          id={itemData.item.id}
+          onDeleteItem={deleteGoalHandler}/>;
            }}
            keyExtractor={(item, index) =>{
               return item.id;
--- Project1/Components/GoalItem.js
+++ Project1/Components/GoalItem.js
@@ -1,12 +1,18 @@
 import {StyleSheet, View, Text, Pressable} from 'react-native'
 
 function GoalItem (props) {
+ 
+
   return (
-    <Pressable onPress={props.onDeleteItem}>
     <View style={styles.goalItem}>
+      <Pressable 
+      android_ripple={{color: '#40195b',}}
+      onPress={props.onDeleteItem.bind(this, props.id)}
+      style={({pressed})=> pressed && styles.pressedItem}
+      >
       <Text style={styles.goalText}>{props.text}</Text> 
+      </Pressable>
   </View>
-  </Pressable>
   )
 };
 
@@ -16,13 +22,17 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
+    
     borderRadius:6,
     backgroundColor: '#5e0acc',
     color: 'white',
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
       color:'white',
+      padding: 8,
     },
   
 })
