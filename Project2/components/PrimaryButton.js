13df5d3fcbc4576d858fe07751a02de913d70950
ambi hidalgo <ambio.pk@gmail.com>
2023-06-30 15:00:14 -0700

    Add props to primary button , check for valid number

--- Project2/components/PrimaryButton.js
+++ Project2/components/PrimaryButton.js
@@ -1,9 +1,7 @@
 import {View, Text, Pressable, StyleSheet} from 'react-native';
 
-function PrimaryButton ({children}) {
-  function pressHandler() {
-    console.log('Pressed!');
-  }
+function PrimaryButton ({children, onPress}) {
+ 
   return( 
     
   <View style={styles.buttonOuterContainer}>
@@ -12,7 +10,7 @@ function PrimaryButton ({children}) {
       pressed 
         ? [styles.buttonInnerContainer, styles.pressed]
         : styles.buttonInnerContainer}
-    onPress={pressHandler} 
+    onPress={onPress} 
     android_ripple={{color: '#73063c'}}
     
     >
