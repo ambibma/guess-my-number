6a2657c8afd2537db21d41bf6f663a5b5c691861
ambi hidalgo <ambio.pk@gmail.com>
2023-07-03 16:32:47 -0700

    Change colors to use reusable color obj

--- Project2/screens/StartGameScreen.js
+++ Project2/screens/StartGameScreen.js
@@ -1,7 +1,10 @@
 import {TextInput, View, StyleSheet, Alert} from 'react-native'
 import { useState } from 'react';
-import PrimaryButton from '../components/PrimaryButton';
-function StartGameScreen() {
+import PrimaryButton from '../components/ui/PrimaryButton';
+import Colors from '../constants/Colors';
+
+
+function StartGameScreen({onPickNumber}) {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   function numberInputHandler(enteredText) {
@@ -24,7 +27,8 @@ function StartGameScreen() {
         );
         return; 
       }
-      return  console.log('Valid Number!');
+      onPickNumber(chosenNumber);
+      
   }
 
   return (
@@ -62,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#52032a',
+    backgroundColor: Colors.primary800,
     borderRadius: 8,
     elevation: 8,
     shadowColor: 'black',
@@ -74,9 +78,9 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor: '#ddb52f',
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: '#ddb52f',
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: 'center',
