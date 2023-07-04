a25793f93caa49b66815430ec3f20e42debf71d6
ambi hidalgo <ambio.pk@gmail.com>
2023-07-03 20:55:24 -0700

    Add fonts, gameOverState, pickNumber

--- Project2/App.js
+++ Project2/App.js
@@ -1,21 +1,37 @@
 import { useState } from 'react'
 import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-
+import { useFonts } from 'expo-font'
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import Colors from './constants/Colors';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
+
   const [userNumber, setUserNumber] = useState(null);
+  const [gameIsOver, setGameIsOver] = useState(true);
+
+  useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
 
   function pickedNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber)
+    setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+  function gameOverHandler() {
+    setGameIsOver(true);
   }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>
     if(userNumber){
-      screen = <GameScreen/>
+      screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>
+    }
+
+    if(gameIsOver && userNumber){
+      screen = <GameOverScreen />
     }
 
   return (
